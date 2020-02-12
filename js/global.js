5091ec3b653a8e7f16dc0c452e5eff7da7cb87d7
mani96 <manvirbhatti96@gmail.com>
2020-02-11 22:40:16 -0800

    portfolio files

---
+++ js/global.js
@@ -0,0 +1,186 @@
+
+$(document).ready(function() {
+var html = Circles.create({
+            id: 'html',
+            radius: 60,
+            value: 95,
+            maxValue: 100,
+            width: 10,
+            text: function (value) {
+                return value + '%';
+            },
+            colors: ['#898989', '#fc8002'],
+            duration: 700,
+            wrpClass: 'circles-wrp',
+            textClass: 'circles-text',
+            valueStrokeClass: 'circles-valueStroke',
+            maxValueStrokeClass: 'circles-maxValueStroke',
+            styleWrapper: true,
+            styleText: true
+        });
+		var css = Circles.create({
+            id: 'css',
+            radius: 60,
+            value: 90,
+            maxValue: 100,
+            width: 10,
+            text: function (value) {
+                return value + '%';
+            },
+            colors: ['#898989', '#fc8002'],
+            duration: 700,
+            wrpClass: 'circles-wrp',
+            textClass: 'circles-text',
+            valueStrokeClass: 'circles-valueStroke',
+            maxValueStrokeClass: 'circles-maxValueStroke',
+            styleWrapper: true,
+            styleText: true
+        });
+        var javascript = Circles.create({
+            id: 'javascript',
+            radius: 60,
+            value: 55,
+            maxValue: 100,
+            width: 10,
+            text: function (value) {
+                return value + '%';
+            },
+            colors: ['#898989', '#fc8002'],
+            duration: 700,
+            wrpClass: 'circles-wrp',
+            textClass: 'circles-text',
+            valueStrokeClass: 'circles-valueStroke',
+            maxValueStrokeClass: 'circles-maxValueStroke',
+            styleWrapper: true,
+            styleText: true
+        });
+        var jquery = Circles.create({
+            id: 'jquery',
+            radius: 60,
+            value: 50,
+            maxValue: 100,
+            width: 10,
+            text: function (value) {
+                return value + '%';
+            },
+            colors: ['#898989', '#fc8002'],
+            duration: 700,
+            wrpClass: 'circles-wrp',
+            textClass: 'circles-text',
+            valueStrokeClass: 'circles-valueStroke',
+            maxValueStrokeClass: 'circles-maxValueStroke',
+            styleWrapper: true,
+            styleText: true
+        });
+        var java = Circles.create({
+            id: 'java',
+            radius: 60,
+            value: 70,
+            maxValue: 100,
+            width: 10,
+            text: function (value) {
+                return value + '%';
+            },
+            colors: ['#898989', '#fc8002'],
+            duration: 700,
+            wrpClass: 'circles-wrp',
+            textClass: 'circles-text',
+            valueStrokeClass: 'circles-valueStroke',
+            maxValueStrokeClass: 'circles-maxValueStroke',
+            styleWrapper: true,
+            styleText: true
+        });
+        var php = Circles.create({
+            id: 'php',
+            radius: 60,
+            value: 80,
+            maxValue: 100,
+            width: 10,
+            text: function (value) {
+                return value + '%';
+            },
+            colors: ['#898989', '#fc8002'],
+            duration: 700,
+            wrpClass: 'circles-wrp',
+            textClass: 'circles-text',
+            valueStrokeClass: 'circles-valueStroke',
+            maxValueStrokeClass: 'circles-maxValueStroke',
+            styleWrapper: true,
+            styleText: true
+        });
+        var Csharp = Circles.create({
+            id: 'csharp',
+            radius: 60,
+            value: 75,
+            maxValue: 100,
+            width: 10,
+            text: function (value) {
+                return value + '%';
+            },
+            colors: ['#898989', '#fc8002'],
+            duration: 700,
+            wrpClass: 'circles-wrp',
+            textClass: 'circles-text',
+            valueStrokeClass: 'circles-valueStroke',
+            maxValueStrokeClass: 'circles-maxValueStroke',
+            styleWrapper: true,
+            styleText: true
+        });
+        var python = Circles.create({
+            id: 'python',
+            radius: 60,
+            value: 40,
+            maxValue: 100,
+            width: 10,
+            text: function (value) {
+                return value + '%';
+            },
+            colors: ['#898989', '#fc8002'],
+            duration: 700,
+            wrpClass: 'circles-wrp',
+            textClass: 'circles-text',
+            valueStrokeClass: 'circles-valueStroke',
+            maxValueStrokeClass: 'circles-maxValueStroke',
+            styleWrapper: true,
+            styleText: true
+        });
+		
+		 var sql = Circles.create({
+            id: 'sql',
+            radius: 60,
+            value: 80,
+            maxValue: 100,
+            width: 10,
+            text: function (value) {
+                return value + '%';
+            },
+            colors: ['#898989', '#fc8002'],
+            duration: 700,
+            wrpClass: 'circles-wrp',
+            textClass: 'circles-text',
+            valueStrokeClass: 'circles-valueStroke',
+            maxValueStrokeClass: 'circles-maxValueStroke',
+            styleWrapper: true,
+            styleText: true
+        });
+		 var android = Circles.create({
+            id: 'android',
+            radius: 60,
+            value: 65,
+            maxValue: 100,
+            width: 10,
+            text: function (value) {
+                return value + '%';
+            },
+            colors: ['#898989', '#fc8002'],
+            duration: 700,
+            wrpClass: 'circles-wrp',
+            textClass: 'circles-text',
+            valueStrokeClass: 'circles-valueStroke',
+            maxValueStrokeClass: 'circles-maxValueStroke',
+            styleWrapper: true,
+            styleText: true
+        });
+    
+});
+
